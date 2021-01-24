bb0e6d829a08ee76f09184a8deb9a6ab729c60ba
hyroky867 <handm755@gmail.com>
2021-01-24 21:59:31 +0900

    feat: 1.2.6 チャットルームの生成と利用

--- app/chat/main.go
+++ app/chat/main.go
@@ -22,8 +22,12 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// ルート
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// チャットルームを開始します
+	go r.run()
 
 	// WEb サーバーを開始します
 	if err := http.ListenAndServe(":8080", nil); err != nil {
