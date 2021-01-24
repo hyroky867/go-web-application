eb75e6f31aa0c993786f4d8a095e61d9edb44df8
hyroky867 <handm755@gmail.com>
2021-01-24 21:32:00 +0900

    feat: 1.2.3 goの並列処理

--- app/chat/room.go
+++ app/chat/room.go
@@ -13,3 +13,29 @@ type room struct {
 	// clients には在籍している術とのクライアントが保持されます。
 	clients map[*client]bool
 }
+
+func (r *room) run() {
+	// goroutineであれば他に影響がないため無限ループでも問題ない
+	for {
+		select {
+		case client := <-r.join:
+			// 参加
+			r.clients[client] = true
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// すべてのクライアントにメッセージを転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// メッセージを送信
+				default:
+					// 送信に失敗
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
