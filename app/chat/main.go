d597df3bb890a1b1d373996842c9dcaa05b90c10
hyroky867 <handm755@gmail.com>
2021-01-24 21:04:37 +0900

    feat: 1.1

--- app/chat/main.go
+++ app/chat/main.go
@@ -3,21 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html lang="en">
-				<head>
-					<title>チャット</title>
-				</head>
-				<body>
-					チャットしましょう！
-				</body>
-			</html>
-		`))
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	temp1    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.temp1 = template.Must(template.ParseFiles(filepath.Join("chat/templates", t.filename)))
 	})
+	t.temp1.Execute(w, nil)
+}
+
+func main() {
+	// ルート
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// WEb サーバーを開始します
 	if err := http.ListenAndServe(":8080", nil); err != nil {
