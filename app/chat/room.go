ea03c9b2c3777baa1996d8ad93af5212c71d98c4
hyroky867 <handm755@gmail.com>
2021-01-24 21:24:31 +0900

    feat: 1.2.2 チャットルームのモデル化

--- app/chat/room.go
+++ app/chat/room.go
@@ -3,4 +3,13 @@ package main
 type room struct {
 	// forwardは他のクライアントに転送するためのメッセージを保持するチャネルです。
 	forward chan []byte
+
+	// joinはチャットルームに参加しようとしているクライアントのためのチャネルです。
+	join chan *client
+
+	// leaveはチャットルームから退室しようとしているクライアントのチャネルです。
+	leave chan *client
+
+	// clients には在籍している術とのクライアントが保持されます。
+	clients map[*client]bool
 }
