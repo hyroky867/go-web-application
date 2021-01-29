e425d6064978b1ba64448323848acb6ed5a3b3ae
hyroky867 <handm755@gmail.com>
2021-01-29 22:04:51 +0900

    feat: 3.3.3 アップロードされたファイルの処理

--- app/chapter3/chat/main.go
+++ app/chapter3/chat/main.go
@@ -60,6 +60,7 @@ func main() {
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	})
 	http.Handle("/upload", &templateHandler{filename: "upload.html"})
+	http.HandleFunc("/uploader", uploaderHandler)
 
 	// チャットルームを開始します
 	go r.run()
