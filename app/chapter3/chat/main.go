016282404b925de62f9bd29bd2464ca486b2d79a
hyroky867 <handm755@gmail.com>
2021-01-29 22:13:14 +0900

    feat: 3.3.4 画像の提供

--- app/chapter3/chat/main.go
+++ app/chapter3/chat/main.go
@@ -61,6 +61,11 @@ func main() {
 	})
 	http.Handle("/upload", &templateHandler{filename: "upload.html"})
 	http.HandleFunc("/uploader", uploaderHandler)
+	http.Handle("/avatars/",
+		http.StripPrefix("/avatars/",
+			http.FileServer(http.Dir("./avatars")),
+		),
+	)
 
 	// チャットルームを開始します
 	go r.run()
