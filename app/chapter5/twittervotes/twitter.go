c71500c36476b2424a0e93040598bb71a2aaf975
hyroky867 <handm755@gmail.com>
2021-02-03 22:32:22 +0900

    feat: 5.3.1.2 環境変数の読み込み

---
+++ app/chapter5/twittervotes/twitter.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/garyburd/go-oauth/oauth"
+	"github.com/joho/godotenv"
+)
+
+var conn net.Conn
+
+func dial(netw, addr string) (net.Conn, error) {
+	if conn != nil {
+		conn.Close()
+		conn = nil
+	}
+	netc, err := net.DialTimeout(netw, addr, 5*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	conn = netc
+	return netc, nil
+}
+
+var reader io.ReadCloser
+
+func closeConn() {
+	if conn != nil {
+		conn.Close()
+	}
+	if reader != nil {
+		reader.Close()
+	}
+}
+
+var (
+	authClient *oauth.Client
+	creds      *oauth.Credentials
+)
+
+func setupTwitterAuth() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	creds = &oauth.Credentials{
+		Token:  os.Getenv("SP_TWITTER_ACCESSTOKEN"),
+		Secret: os.Getenv("SP_TWITTER_ACCESSSECRET"),
+	}
+	authClient = &oauth.Client{
+		Credentials: oauth.Credentials{
+			Token:  os.Getenv("SP_TWITTER_KEY"),
+			Secret: os.Getenv("SP_TWITTER_SECRET"),
+		},
+	}
+}
+
+var (
+	authSetupOnce sync.Once
+	httpClient    *http.Client
+)
+
+func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
+	authSetupOnce.Do(func() {
+		setupTwitterAuth()
+		httpClient = &http.Client{
+			Transport: &http.Transport{
+				Dial: dial,
+			},
+		}
+	})
+	formEnc := params.Encode()
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Length", strconv.Itoa(len(formEnc)))
+	req.Header.Set("Authorization", authClient.AuthorizationHeader(creds, "POST", req.URL, params))
+
+	return httpClient.Do(req)
+}
