a78ae28bb849c37dc8a1288d9cb1e23d59436e18
hyroky867 <handm755@gmail.com>
2021-02-04 22:24:46 +0900

    feat: 5.4.4 Ctrl + C への応答

--- app/chapter5/twittervotes/counter/main.go
+++ app/chapter5/twittervotes/counter/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 
 	"github.com/nsqio/go-nsq"
@@ -96,4 +98,17 @@ func main() {
 		}
 		updater.Reset(updateDuration)
 	})
+
+	termChan := make(chan os.Signal, 1)
+	signal.Notify(termChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	for {
+		select {
+		case <-termChan:
+			updater.Stop()
+			q.Stop()
+		case <-q.StopChan:
+			// 完了しました
+			return
+		}
+	}
 }
