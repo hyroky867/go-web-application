0629edecc3a47d599bc3093ed5acb9e7ec05d12a
hyroky867 <handm755@gmail.com>
2021-02-07 15:48:34 +0900

    feat: 6.7.2.1 調査項目の読み込み

--- app/chapter6/api/polls.go
+++ app/chapter6/api/polls.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"net/http"
 
+	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -31,8 +32,26 @@ func handlePolls(w http.ResponseWriter, r *http.Request) {
 }
 
 func handlepollsGet(w http.ResponseWriter, r *http.Request) {
+	db := GetVars(r, "db").(*mgo.Database)
+	c := db.C("polls")
+	var q *mgo.Query
+	p := NewPath(r.URL.Path)
+	if p.HasID() {
+		// 特定の調査項目の詳細
+		q = c.FindId(bson.ObjectIdHex(p.ID))
+	} else {
+		// すべての調査項目のリスト
+		q = c.Find(nil)
+	}
+
+	var result []*poll
+	if err := q.All(&result); err != nil {
+		respondErr(w, r, http.StatusInternalServerError, err)
+		return
+	}
 	respondErr(w, r, http.StatusInternalServerError, errors.New("未実装です"))
 }
+
 func handlepollsPost(w http.ResponseWriter, r *http.Request) {
 	respondErr(w, r, http.StatusInternalServerError, errors.New("未実装です"))
 }
