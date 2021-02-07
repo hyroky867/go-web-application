f5211fc3e1a90e871783a0b878004ffb93a68c83
hyroky867 <handm755@gmail.com>
2021-02-07 18:48:38 +0900

    feat: 8.3.1 少量のデータの永続化

---
+++ app/chapter8/backup/cmds/backup/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"log"
+
+	"github.com/matryer/filedb"
+)
+
+func main() {
+	var fatalErr error
+	defer func() {
+		if fatalErr != nil {
+			flag.PrintDefaults()
+			log.Fatalln(fatalErr)
+		}
+	}()
+
+	var (
+		dbpath = flag.String("db", "./backupdata", "データベースのディレクトリのパス")
+	)
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
+		fatalErr = errors.New("えらー; コマンドを指定してください")
+		return
+	}
+
+	db, err := filedb.Dial(*dbpath)
+	if err != nil {
+		fatalErr = err
+		return
+	}
+	defer db.Close()
+
+	col, err := db.C("paths")
+	if err != nil {
+		fatalErr = err
+		return
+	}
+}
