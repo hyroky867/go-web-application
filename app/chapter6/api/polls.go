8c3672b17b48fd6a574249bffd3ac882354f8089
hyroky867 <handm755@gmail.com>
2021-02-07 15:38:42 +0900

    feat: 6.7.2 ひとつのハンドラで多くの処理を行う

--- app/chapter6/api/polls.go
+++ app/chapter6/api/polls.go
@@ -1,10 +1,41 @@
 package main
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"errors"
+	"net/http"
+
+	"gopkg.in/mgo.v2/bson"
+)
 
 type poll struct {
 	ID      bson.ObjectId  `bson:"_id" json:"id"`
 	Title   string         `json:"title"`
 	Options []string       `json:"options"`
 	Results map[string]int `json:"results,omitempty"`
+}
+
+func handlePolls(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		handlepollsGet(w, r)
+		return
+	case "POST":
+		handlepollsPost(w, r)
+		return
+	case "DELETE":
+		handlepollsDelete(w, r)
+		return
+	}
+	// 未対応のHTTP利用メソッド
+	respondHTTPErr(w, r, http.StatusNotFound)
+}
+
+func handlepollsGet(w http.ResponseWriter, r *http.Request) {
+	respondErr(w, r, http.StatusInternalServerError, errors.New("未実装です"))
+}
+func handlepollsPost(w http.ResponseWriter, r *http.Request) {
+	respondErr(w, r, http.StatusInternalServerError, errors.New("未実装です"))
+}
+func handlepollsDelete(w http.ResponseWriter, r *http.Request) {
+	respondErr(w, r, http.StatusInternalServerError, errors.New("未実装です"))
 }
