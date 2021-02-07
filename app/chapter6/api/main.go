da4e58ac56aaadb6a358fcd0f42d616e397b823d
hyroky867 <handm755@gmail.com>
2021-02-07 14:44:27 +0900

    feat: 6.3 ラップされたハンドラ関数

---
+++ app/chapter6/api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "net/http"
+
+func main() {
+}
+
+func withAPIkey(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !isValidAPIKey(r.URL.Query().Get("key")) {
+			respondErr(w, r, http.StatusUnauthorized, "不正なAPIキーです")
+			return
+		}
+		fn(w, r)
+	}
+}
+
+func isValidAPIKey(key string) bool {
+	return key == "abc123"
+}
